e481dade2a0e7e88e7569f67efa0565d15bd6518
Beatriz <beatrizcazatti@gmail.com>
2024-07-08 20:07:36 -0300

    add exercicio em python

--- Python/ex008-variáveis compostas/02desafio-tabela brasileirão.py
+++ Python/ex008-variáveis compostas/02desafio-tabela brasileirão.py
@@ -4,3 +4,15 @@
     #C) Uma lista com os times em ordem alfabética.
     #D) Em que posição na tabela está o time da Chapecoense.
 
+tabela = ("Vasco da Gama", "Corinthians", "Palmeiras", "São Paulo", "Santos", "Internacional", "Grêmio", "Cruzeiro", "Atlético Mineiro", "Fluminense", "Botafogo", "Flamengo", "Bahia", "Sport Recife", "Ceará", "Fortaleza", "Athletico Paranaense", "Coritiba", "Goiás", "Chapecoense")
+
+print(f'A Tabela de times do Brasileirão é: {tabela}')
+print('=-='*10)
+print(f'Os 5 primeiros colocados são: {tabela[0:6]}')
+print('=-='*10)
+print(f'Os últimos 4 colocados da tabela: {tabela[-4:]}')
+print('=-='*10)
+print(f'Times em orem alfabética: {sorted(tabela)}')
+print('=-='*10)
+print(f'A Chapecoense está na {tabela.index("Chapecoense")}ª posição')
+print('=-='*10)
