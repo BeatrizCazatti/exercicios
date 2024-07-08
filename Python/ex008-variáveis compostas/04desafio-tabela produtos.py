a051232be3d1b894bc6f0a2b279ef8ea4073ced5
Beatriz <beatrizcazatti@gmail.com>
2024-07-08 20:31:23 -0300

    add exercicio em python

--- Python/ex008-variáveis compostas/04desafio-tabela produtos.py
+++ Python/ex008-variáveis compostas/04desafio-tabela produtos.py
@@ -5,10 +5,16 @@ lista = ('Lápis', 1.85,
         'Borracha', 2,
         'Caderno', 5.50, 
         'Régua', 3.20, 
-        'Caneta', 1.50 
+        'Caneta', 1.50 ,
         'Mochila', 80.00, 
         'Estojo', 10.50)
+print('-'*40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-'*40)
 
-for pos in lista:
+for pos in range(0, len(lista)):
     if pos % 2 == 0:
-        print(f'{lista[pos]:.<30}')
+        print(f'{lista[pos]:.<30}', end='')
+    else:
+        print(f'R${lista[pos]:>5}')
+print('-'*40)
