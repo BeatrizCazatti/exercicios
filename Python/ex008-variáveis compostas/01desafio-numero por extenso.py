abcd88de96bfd23acd79257cca1241af12c4c196
Beatriz <beatrizcazatti@gmail.com>
2024-07-08 19:37:43 -0300

    add exercicio em python

--- Python/ex008-variáveis compostas/01desafio-numero por extenso.py
+++ Python/ex008-variáveis compostas/01desafio-numero por extenso.py
@@ -1,2 +1,10 @@
 #desafio 01: Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
     #Seu programa deverá ler um número pelo teclado (entre 0 a 20) a mostrá-lo por extenso.
+
+numerosExtenso = ('Zero','Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+num = int(input('Digite um número de 0 a 20:'))
+
+for c in range(0, 20):
+    if num == c:
+        print(f'{numerosExtenso[c]}')
+        break
