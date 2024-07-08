b3d3d3f5cd196a67446428845c7c761d8a8ec602
Beatriz <beatrizcazatti@gmail.com>
2024-07-08 20:20:16 -0300

    add exercicio em python

--- Python/ex008-variáveis compostas/03desafio-num aleatorios.py
+++ Python/ex008-variáveis compostas/03desafio-num aleatorios.py
@@ -1,2 +1,13 @@
 #desafio 03: Cria um programa que vai gerar cinco números aleatórios a colocar em uma tupla.
     #Depois disso, mostra a listagem de números gerados e também indique o menor a o maior valor que estão na tupla.
+
+from random import randint
+
+num1 = int(randint(0,9))
+num2 = int(randint(0,9))
+num3 = int(randint(0,9))
+num4 = int(randint(0,9))
+num5 = int(randint(0,9))
+numeros = (num1, num2, num3, num4, num5)
+print(numeros)
+print(f'O maior é {sorted(numeros)[4]} e o menor é {sorted(numeros)[0]}.')
--- Python/ex008-variáveis compostas/04desafio-tabela produtos.py
+++ Python/ex008-variáveis compostas/04desafio-tabela produtos.py
@@ -1,2 +1,14 @@
 #desafio 04: Crie um programa que tenha uma tupla única com nomes de produtos a seus respectivos preços, na sequência.
     #No final, mostra uma listagem de preços, organizando os dados em forma tabular.
+
+lista = ('Lápis', 1.85, 
+        'Borracha', 2,
+        'Caderno', 5.50, 
+        'Régua', 3.20, 
+        'Caneta', 1.50 
+        'Mochila', 80.00, 
+        'Estojo', 10.50)
+
+for pos in lista:
+    if pos % 2 == 0:
+        print(f'{lista[pos]:.<30}')
