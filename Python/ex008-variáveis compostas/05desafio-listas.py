ef3ba6660d12c10622ba4b04c19d7bf5df921d03
Beatriz <beatrizcazatti@gmail.com>
2024-07-15 20:12:28 -0300

    add exercícios em python

--- Python/ex008-variáveis compostas/05desafio-listas.py
+++ Python/ex008-variáveis compostas/05desafio-listas.py
@@ -1,2 +1,17 @@
 #desafio 05: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
     #No final, mostre qual foi o maior a o menor valor digitado a as suas respectivas posições na lista.
+
+lista = []
+
+for n in range(0, 5):
+    lista.append(int(input('Número:')))
+print(lista)
+
+listaOrdenada = lista[:]
+listaOrdenada.sort()
+
+menor = listaOrdenada[0]
+maior = listaOrdenada[len(lista) - 1]
+
+print(f'O menor é {menor} que está na posição {lista.index(menor)}')
+print(f'O maior é {maior} que está na posição {lista.index(maior)}')
--- Python/ex008-variáveis compostas/07desafio-.py
+++ Python/ex008-variáveis compostas/07desafio-.py
@@ -1,2 +1,3 @@
 #desafio 07: Crie um programa onde o usuário digita uma expressão qualquer que use parenteses. 
     #Seu aplicativo deverá analisar se a expressão passada está com os parêntesis abertos a fechados na ordem correta.
+
