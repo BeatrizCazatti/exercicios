c5f27073e38f2856c216052d3c43f66f3f85c3ca
Beatriz <beatrizcazatti@gmail.com>
2024-07-22 20:22:10 -0300

    add exercícios

--- Python/ex008-variáveis compostas/09desafio-listas com pares e impares.py
+++ Python/ex008-variáveis compostas/09desafio-listas com pares e impares.py
@@ -1,1 +1,18 @@
-#desafio 09: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares a ímpares. No final. mostre os valores parrs e ímpares em ordem crescente.
+#desafio 09: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares a ímpares. No final mostre os valores pares e ímpares em ordem crescente.
+
+pares = []
+impares = []
+num = 0
+
+for n in range(1, 8):
+    num = int(input(f'Digite o {n}º valor:'))
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
+    
+pares.sort()
+impares.sort()
+lista = [pares[:], impares[:]]
+print(f'Os valores pares digitados foram {lista[0]}.')
+print(f'Os valores ímpares digitados foram {lista[1]}.')
