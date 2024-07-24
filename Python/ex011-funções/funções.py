6620cd56db22bbc786a1d0a32291157adab9714a
Beatriz <beatrizcazatti@gmail.com>
2024-07-24 09:50:20 -0300

    add exercícios

--- Python/ex011-funções/funções.py
+++ Python/ex011-funções/funções.py
@@ -1,5 +1,18 @@
+def lin(txt):
+    tam = len(txt) + 4
+    print('-'*tam)
+    print(f'  {txt.upper():^}  ')
+    print('-'*tam)
 def soma(a, b):
     print(a + b)
+def contador(* num):
+    print(f'Recebi {len(num)} números, são eles {num}')
 
+lin('soma')
 soma(3, 5)
 soma(a=12, b=3)
+
+lin('contador')
+contador(2, 4, 8)
+contador(1, 6)
+contador(3, 7, 9, 2)
