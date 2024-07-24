a34d8584f89d3f6d62d75f9b59bd1fb9bb64cdd8
Beatriz <beatrizcazatti@gmail.com>
2024-07-23 23:21:10 -0300

    add exercícios

--- Python/ex008-variáveis compostas/15desafio-cadastro jogador.py
+++ Python/ex008-variáveis compostas/15desafio-cadastro jogador.py
@@ -1,1 +1,21 @@
 #desafio 15: Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+
+jogador = {}
+
+nome = str(input('Nome: '))
+jogador['nome'] = nome
+
+partidas = int(input(f' Quantas partidas {nome} jogou? '))
+gols = []
+total = 0
+
+for n in range(1, partidas +1):
+    num = int(input(f'Gols feitos no {n}ºjogo: '))
+    gols.append(num)
+    total += num
+jogador['gols'] = gols
+jogador['total'] = total
+
+print('=-='*20)
+print(jogador)
+print('=-='*20)
