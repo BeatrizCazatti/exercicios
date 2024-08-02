f65184ed0b90d888ba3a273050a4289660da8f82
Beatriz <beatrizcazatti@gmail.com>
2024-08-02 20:50:20 -0300

    add exercícios

---
+++ Python/ex011-funções/05desafio-função fatorial.py
@@ -0,0 +1,13 @@
+#desafio 05: Crie um programa que tenha uma função fatorial() que raceba dois parâmetros: o primeiro que indique o número a calcular a o outro chamado show. que será um valor lógico (opcional) indicando sa será mostrado ou não na tela o processo de cálculo do fatorial.
+
+def fatorial(num, show):
+    result = 1
+    
+    while num != 0:
+        if show:
+            print(f' {num} X' if num > 1 else f' {num} = ', end='')
+        result *= num
+        num -= 1
+    return result
+
+print(fatorial(int(input('Digite um número para calcular seu fatorial:')), False))
