e39c89bf0cc65cbe8b1bf7664d936ef442a1438e
Beatriz <beatrizcazatti@gmail.com>
2024-08-03 15:23:07 -0300

    add exercícios

--- Python/ex012-módulos e pacotes/01desafio-main.py
+++ Python/ex012-módulos e pacotes/01desafio-main.py
@@ -1,8 +1,14 @@
 #desafio 01:Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
 #Faça também um programa que importe essa módulo a use algumas dessas funções.
 
-
 #01.3: Modifique as funções que foram criadas no desafio 01 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda().
 
+import moeda
+valor = int(input('Digite um preço: '))
+
+print(f'O aumento de 10% é {moeda.aumentar(valor, 10)}')
+print(f'A redução de 13% é {moeda.diminuir(valor, 13)}')
+print(f'O dobro de {valor} é {moeda.dobro(valor)}')
+print(f'A metade de {valor} é {moeda.metade(valor)}')
 
 
--- Python/ex012-módulos e pacotes/moeda.py
+++ Python/ex012-módulos e pacotes/moeda.py
@@ -1,19 +1,20 @@
-def aumentar(p, a):
-    global valor
-    valor = valor + aumento
+def aumentar(preço, taxa):
+    return preço + (preço * taxa/100)
 
-def diminuir(p, d):
+def diminuir(preço, taxa):
+    return preço - (preço * taxa/100)
 
+def dobro(preço):
+    return preço*2
 
-def dobro(p):
-
-def metade(p):
+def metade(preço):
+    return preço/2
 
 #01.2: Adapte o código do desafio anterior, criando uma função adicional chamada moeda() que consiga mostrar os valores como um valor monetário formatado.
 
-def moeda(v):
+# def moeda(v):
 
 
 #01.4: Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como uma função input(), mas com uma validação de dados para aceitar apenas valores que sejam monetários.
 
-def leiaDinheiro(v)
+# def leiaDinheiro(v)
