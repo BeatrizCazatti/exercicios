a6c51f46651eb04a66dbf04d42045f4ddf1def95
Beatriz <beatrizcazatti@gmail.com>
2024-08-03 15:10:54 -0300

    add exercícios

---
+++ Python/ex012-módulos e pacotes/01desafio-main.py
@@ -0,0 +1,8 @@
+#desafio 01:Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
+#Faça também um programa que importe essa módulo a use algumas dessas funções.
+
+
+#01.3: Modifique as funções que foram criadas no desafio 01 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda().
+
+
+
---
+++ Python/ex012-módulos e pacotes/modulo-desafio/moeda.py
@@ -0,0 +1,19 @@
+def aumentar(p, a):
+    global valor
+    valor = valor + aumento
+
+def diminuir(p, d):
+
+
+def dobro(p):
+
+def metade(p):
+
+#01.2: Adapte o código do desafio anterior, criando uma função adicional chamada moeda() que consiga mostrar os valores como um valor monetário formatado.
+
+def moeda(v):
+
+
+#01.4: Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como uma função input(), mas com uma validação de dados para aceitar apenas valores que sejam monetários.
+
+def leiaDinheiro(v)
